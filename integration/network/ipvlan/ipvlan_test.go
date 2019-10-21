tiborvass/docker

Integration: IPVlan add missing import comment
Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

integration/network/ipvlan/ipvlan_test.go
@@ -1,6 +1,6 @@
 // +build !windows
 
-package ipvlan
+package ipvlan // import "github.com/tiborvass/docker/integration/network/ipvlan"
 
 import (
 	"context"

burnMyDread/moby

Signed-off-by: zach <Zachary.Joyner@linux.com>

integration/network/ipvlan/ipvlan_test.go
@@ -1,6 +1,6 @@
 // +build !windows
 
-package ipvlan
+package ipvlan // import "github.com/docker/docker/integration/network/ipvlan"
 
 import (
 	"context"